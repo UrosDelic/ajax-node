d6b08724752093a00dfe2fe6709f94100bce9f02
uros_delic <uros.delic@quantox.com>
2021-05-24 09:43:49 +0200

    backend cleanup

--- BackEnd.js
+++ BackEnd.js
@@ -1,23 +1,27 @@
-
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-    response.setHeader("Access-Control-Allow-Origin", '*');
-    response.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    response.setHeader('content-type', 'application/json');
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.write("<h2>server is working</h2>");
-    response.end();
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, OPTIONS, PUT, DELETE"
+  );
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Accept"
+  );
+  response.writeHead(200, { "Content-type": "text/html" });
+  response.write("<h2>server is working</h2>");
+  response.end();
 });
 
 const host = "localhost";
 const port = 3000;
 
 server.listen(port, host, error => {
-    if (error) {
-        console.log("Something went wrong", error);
-    } else {
-        console.log(`Server is runing on http://${host}:${port}`);
-    }
+  if (error) {
+    console.log("Something went wrong", error);
+  } else {
+    console.log(`Server is runing on http://${host}:${port}`);
+  }
 });
