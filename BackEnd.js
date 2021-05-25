1a902fccaf984eaed43195624d79ee7e58881e93
uros_delic <uros.delic@quantox.com>
2021-05-25 12:52:53 +0200

    radi post radi get

--- BackEnd.js
+++ BackEnd.js
@@ -2,29 +2,29 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((request, response) => {
+  // cors
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+
   if (request.method === "POST") {
     let data = [];
     request
-      .on("data", payload => {
-        data.push(payload);
+      .on("data", payloadChunk => {
+        data.push(payloadChunk);
       })
       .on("end", () => {
         data = Buffer.concat(data).toString();
-        console.log("data buffer", data);
         fs.writeFile("message.json", data, () => {});
       });
+  } else if (request.method === "GET") {
+    let message = fs.readFileSync("message.json", () => {});
+    message = Buffer.from(message);
+    response.write(message);
   }
-  // nasao sam na stack-u, za CORS
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Accept"
-  );
-
-  response.writeHead(200, { "Content-type": "text/html" });
-  response.write("This is the response from server!");
   response.end();
 });
+
 const host = "localhost";
 const port = 3000;
 
--- FrontEnd.js
+++ FrontEnd.js
@@ -2,19 +2,30 @@ const httpRequest = new XMLHttpRequest();
 
 const obj = {
   vehicles: {
-    car: "car",
-    bike: "bike",
-    plane: "plane",
+    car: "OPET TEST",
+    bike: "OPET TEST",
+    plane: "OPET TEST",
   },
 };
 const JSONobj = JSON.stringify(obj);
 
-function sendRequest() {
+function postRequest() {
   httpRequest.open("POST", "http://localhost:3000/");
   httpRequest.setRequestHeader("Content-type", "application/json");
-
   httpRequest.send(JSONobj);
 }
 
+function getRequest() {
+  httpRequest.open("GET", "http://localhost:3000/");
+  httpRequest.onload = () => {
+    const data = JSON.parse(httpRequest.response);
+  };
+  // izvan onload mora biti - proveri
+  httpRequest.send();
+}
+
 const postButton = document.getElementById("post-button");
-postButton.addEventListener("click", sendRequest);
+postButton.addEventListener("click", postRequest);
+
+const getButton = document.getElementById("get-button");
+getButton.addEventListener("click", getRequest);
