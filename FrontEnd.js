dc32ad69d1a715f7f08db444b8b1f81f74b72474
uros_delic <uros.delic@quantox.com>
2021-05-26 16:27:09 +0200

    refactor i novi komentari

--- FrontEnd.js
+++ FrontEnd.js
@@ -1,4 +1,4 @@
-const httpRequest = new XMLHttpRequest();
+//const httpRequest = new XMLHttpRequest();
 
 const postButton = document.getElementById("post-button");
 postButton.addEventListener("click", postRequest);
@@ -6,29 +6,44 @@ postButton.addEventListener("click", postRequest);
 const getButton = document.getElementById("get-button");
 getButton.addEventListener("click", getRequest);
 
+// refactor
+const sendHttpRequest = (method, url, data) => {
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.open(method, url);
+  if (data) {
+    httpRequest.setRequestHeader("Content-type", "application/json");
+    httpRequest.send(data);
+  }
+  // nauci onload
+  httpRequest.onload = () => {
+    // httpRequest.response;
+    // console.log(httpRequest.response, "data u onload");
+  };
+  httpRequest.send();
+};
+
 const obj = {
   car: "merc",
   bike: "KTM",
-  plane: "AIRBUS",
-  meal: "PUNJENA",
-  desert: "TORTA",
 };
 const JSONobj = JSON.stringify(obj);
 
 function postRequest() {
-  httpRequest.open("POST", "http://localhost:3000/");
-  httpRequest.setRequestHeader("Content-type", "application/json");
-  httpRequest.send(JSONobj);
+  sendHttpRequest("POST", "http://localhost:3000/", JSONobj);
+  // httpRequest.open("POST", "http://localhost:3000/");
+  // httpRequest.setRequestHeader("Content-type", "application/json");
+  // httpRequest.send(JSONobj);
 }
 
 function getRequest() {
-  httpRequest.open("GET", "http://localhost:3000/");
+  sendHttpRequest("GET", "http://localhost:3000/");
+  //httpRequest.open("GET", "http://localhost:3000/");
   // Do something with the retrieved data
-  httpRequest.onload = () => {
-    let data = JSON.parse(httpRequest.response);
-    console.log("data u onload", data);
-  };
-  httpRequest.send();
+  // httpRequest.onload = () => {
+  //   let data = JSON.parse(httpRequest.response);
+  //   console.log("data u onload", data);
+  // };
+  // httpRequest.send();
 }
 // NAPISI FUNCTION ZA KREIRANJE TABELE SA DATA IZ DOBIJENOG OBJEKTA I POZOVI JE U ONLOAD
 ////
